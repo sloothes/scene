20377609d1eccb7dc3b76d21d5be12659b59d38d
Jane Slooth <sloothes.com@gmail.com>
2019-06-19 15:57:10 +0300

    Update action-helpers.js

--- action-helpers.js
+++ action-helpers.js
@@ -10,6 +10,23 @@
         return a < b ? -a : b;
     }
 
+    function updateDirectionControls(deg){
+
+        try {
+
+        //  Update control tab direction controls.
+        //  "directionOutput" and "directionSlider" 
+        //  has been parsed by control-tab.
+
+            directionOutput.value = mod(deg, 360);
+            directionSlider.value = mod(deg, 360);
+
+        } 
+
+        catch(err){;}
+
+    }
+
     function turnTo( rad, immediate ){
 
         var deltaAngle = getDeltaAngle( localPlayer.controller.direction, rad );
@@ -30,21 +47,13 @@
 
             if ( Math.abs( deltaAngle ) < 0.000017453292519943296  ) {
                 cancelAnimationFrame( windowAnimationFrameRequestID );
-                var deg = parseInt( THREE.Math.radToDeg(localPlayer.controller.direction) );
-
-                try {
-
-                //  Update control tab direction controls.
-                    directionOutput.value = mod(deg, 360);
-                    directionSlider.value = mod(deg, 360);
-                } 
-
-                catch(err){;}
-
+                updateDirectionControls( round( THREE.Math.radToDeg(localPlayer.controller.direction), 0 ) ); // deg.
                 localPlayer.outfit.update(); return;
             }
 
             localPlayer.controller.direction = mod( localPlayer.controller.direction += ( deltaAngle *= 0.5 ), 2 * Math.PI );
+            updateDirectionControls( round( THREE.Math.radToDeg(localPlayer.controller.direction), 0 ) ); // deg.
+
         }
 
     }
