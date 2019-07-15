4ca03d1b6e58dd79a8a0af33069bcf1c89f14c6f
Jane Slooth <sloothes.com@gmail.com>
2019-07-15 13:52:15 +0300

    Update action-helpers.js

--- action-helpers.js
+++ action-helpers.js
@@ -56,5 +56,18 @@
 
         }
 
+    //  round.js  source: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round"
+
+        function round(number, precision) {
+            var shift = function (number, precision, reverseShift) {
+                if (reverseShift) {
+                    precision = -precision;
+                }  
+                numArray = ("" + number).split("e");
+                return +(numArray[0] + "e" + (numArray[1] ? (+numArray[1] + precision) : precision));
+            };
+            return shift(Math.round(shift(number, precision, false)), precision, true);
+        }
+
     }
 
